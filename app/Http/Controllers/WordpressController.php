ab04db147adc6d1025686181eadbc29f71a60206
Jesús Amieiro <amieiro@gmail.com>
2019-12-17 15:28:02 +0000

    Creo la plantilla base a partir de la del listado de posts y añado las opciones de WordPress

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Corcel\Model\Menu;
+use Corcel\Model\Option;
 use Corcel\Model\Post;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,9 @@ class WordpressController extends Controller
 {
     public function index() {
         $menu = Menu::slug('menu-principal')->first();
-        $posts = Post::status('publish')->type('post')->newest()->paginate(5);//->get();
+        $posts = Post::status('publish')->type('post')->newest()->paginate(5);
+        $options = Option::asArray();
 
-        return view('index', compact('menu', 'posts'));
+        return view('index', compact('menu', 'posts', 'options'));
     }
 }
