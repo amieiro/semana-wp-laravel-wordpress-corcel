873112b34946f79b01a4deb47ec65ec0c939e8b0
Jesús Amieiro <amieiro@gmail.com>
2019-12-17 15:18:10 +0000

    Añado el listado de posts y su paginación

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Corcel\Model\Menu;
+use Corcel\Model\Post;
 use Illuminate\Http\Request;
 
 class WordpressController extends Controller
 {
     public function index() {
         $menu = Menu::slug('menu-principal')->first();
-        return view('index', compact('menu'));
+        $posts = Post::status('publish')->type('post')->newest()->paginate(5);//->get();
+
+        return view('index', compact('menu', 'posts'));
     }
 }
